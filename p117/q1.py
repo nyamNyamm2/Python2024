f5db6e5921ee3ed6e4face863a3e91c05234ade6
shimsGram <shims5980@daum.net>
2024-10-08 11:40:22 +0900

    10월 8일

---
+++ p117/q1.py
@@ -0,0 +1,13 @@
+'''
+실습일: 2024.10.08
+실습자: 201910852 심기윤
+실습내용: 모듈 활용
+'''
+
+import math
+
+# math.radians(degree)
+# math.degrees(radian)
+
+print(f'360도 -> 라디안: {math.radians(360) : .4f}')
+print(f'2pi라디안 -> 각도: {math.degrees(2*math.pi) : .4f}')
---
+++ p117/q2.py
@@ -0,0 +1,12 @@
+'''
+실습일: 2024.10.08
+실습자: 201910852 심기윤
+실습내용: 모듈 활용
+'''
+
+import math
+
+# math.sin( math.pi * (value / 180) )
+# math.cos( math.pi * (value / 180) )
+# math.tan( math.pi * (value / 180) )
+print(f'sin(30) + cos(30) = {math.sin(math.pi * (30 / 180)) + math.cos(math.pi * (30 / 180)) : .4f}')
